13fbf717070bc8a6d1e149a2d5e0930732de766a
hamza-hamzii <hmza.ehmad@gmail.com>
2021-05-19 22:21:08 +0500

    folder deleted

--- mxp-library/src/lib-components/index.js
+++
@@ -1,5 +0,0 @@
-/* eslint-disable import/prefer-default-export */
-import mxpButton from './mxpButton.vue';
-import mxpInput from './mxpInput.vue';
-import mxpCard from './mxpCard.vue';
-export { mxpButton, mxpInput, mxpCard };
